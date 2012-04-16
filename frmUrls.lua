39ea36c638c0339a6edbd58e9dc5f4ff4db715da
Fur Q <furq@b23.be>
2012-04-16 05:49:18 +0100

    initial commit

---
+++ frmUrls.lua
@@ -0,0 +1,161 @@
+local frmUrls = {
+	clip = wx.wxClipboard.Get(),
+	tobj = wx.wxTextDataObject(),
+	urls = {},
+	search = {},
+	search_html = {},
+  frame = nil
+}
+
+frmUrls.hfmt = wx.wxDataFormat("HTML Format")
+frmUrls.hobj = wx.wxLuaDataObjectSimple(frmUrls.hfmt)
+frmUrls.hobj.data = ""
+function frmUrls.hobj:GetDataHere() return true, self.data end
+function frmUrls.hobj:GetDataSize() return self.data:len() end
+function frmUrls.hobj:SetData(d) self.data = d return true end
+
+function frmUrls:Show()
+  if not self.frame then self:Init() end
+  self.lstUrls:InsertItems(self.urls, 0)
+  self.frame:Show()
+  self.frame:Raise()
+end
+
+function frmUrls:Init()
+  self.frame = wx.wxFrame(wx.NULL,
+		wx.wxID_ANY,
+		"Select links to open...",
+		wx.wxDefaultPosition,
+		wx.wxSize( 360,300 ),
+		wx.wxCAPTION + wx.wxCLOSE_BOX + wx.wxSYSTEM_MENU + wx.wxTAB_TRAVERSAL
+	)
+	local lframe = self.frame
+	lframe:SetSizeHints( wx.wxDefaultSize, wx.wxDefaultSize )
+	
+	self.vszUrls = wx.wxBoxSizer( wx.wxVERTICAL )
+	
+	self.lstUrls = wx.wxListBox( lframe, wx.wxID_ANY, wx.wxDefaultPosition, wx.wxDefaultSize, {}, wx.wxLB_MULTIPLE )
+	self.vszUrls:Add( self.lstUrls, 1,  bit.bor(wx.wxALL,wx.wxEXPAND), 5 )
+	
+	self.hszButtons = wx.wxBoxSizer( wx.wxHORIZONTAL )
+	
+	self.btnCancel = wx.wxButton( lframe, wx.wxID_ANY, "Cancel", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
+	self.hszButtons:Add( self.btnCancel, 0, wx.wxALL, 5 )
+	
+	self.hszButtons:Add( 0, 0, 1, wx.wxEXPAND, 5 )
+	
+	self.btnSelected = wx.wxButton( lframe, wx.wxID_ANY, "Open selected", wx.wxDefaultPosition, wx.wxDefaultSize, wx.wxBU_EXACTFIT )
+	self.hszButtons:Add( self.btnSelected, 0, wx.wxALL, 5 )
+	
+	self.btnAll = wx.wxButton( lframe, wx.wxID_ANY, "Open all", wx.wxDefaultPosition, wx.wxDefaultSize, 0 )
+	self.hszButtons:Add( self.btnAll, 0, wx.wxALL, 5 )
+		
+	self.vszUrls:Add( self.hszButtons, 0, wx.wxEXPAND, 5 )
+		
+	lframe:SetSizer( self.vszUrls )
+	lframe:Layout()
+	
+	lframe:Centre( wx.wxBOTH )
+
+  -- events
+
+  lframe:Connect(wx.wxEVT_CLOSE_WINDOW, 
+  	function(e) self:Close() end
+  )
+
+	self.btnSelected:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED,
+		function(e)
+		  local _, sel = self.lstUrls:GetSelections()
+		  local urls = {}
+		  for _,k in pairs(sel) do table.insert(urls, self.urls[k]) end
+		  self:OpenUrls(urls)
+		  self:Close()
+		end
+	)
+
+	self.btnAll:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED,
+		function(e)
+		  self:OpenUrls(self.urls)
+		  self:Close()
+		end
+	)
+
+	self.btnCancel:Connect(wx.wxEVT_COMMAND_BUTTON_CLICKED,
+		function(e)
+		  self:Close()
+		end
+	)
+
+end
+
+-- build list of URL patterns to search clipboard for
+function frmUrls:BuildSearch()
+	self.search, self.search_html = {}, {}
+	for _,v in pairs(cfg.prefixes) do 
+		table.insert(self.search, v.."://[^%s\"']+") 
+	end
+	for _,v in pairs(cfg.subdomains) do 
+		table.insert(self.search, v..".[^%s\"']+") 
+	end
+	for _,v in pairs(cfg.attrs) do 
+		for _,w in pairs(self.search) do
+		  table.insert(self.search_html, v.."=[\"']?("..w..")") 
+		end
+	end
+	for k,v in pairs(self.search) do print(k,v) end
+	for k,v in pairs(self.search_html) do print(k,v) end
+end
+
+-- extract URLs from contents of text/HTML on the clipboard
+function frmUrls:GetUrls()
+	-- parse text on clipboard
+	if self.clip:GetData(self.tobj) then
+		local data = self.tobj:GetText()
+		for _,v in pairs(self.search) do
+			for url in data:gmatch(v) do table.insert(self.urls, url) end
+		end
+	end
+  -- parse html on clipboard
+  if self.clip:GetData(self.hobj) then
+		for _,v in pairs(self.search_html) do
+			for url in self.hobj.data:gmatch(v) do table.insert(self.urls, url) end
+		end
+	end
+	for k,v in pairs(self.urls) do
+		-- convert URLs to lowercase
+		if cfg.lower == 1 then
+			if not v:match("[a-z]") then self.urls[k] = v:lower() end
+		elseif cfg.lower == 2 then
+			local u = string.split(v, "/")
+			for i = 1,#u-1 do u[i] = u[i]:lower() end
+			self.urls[k] = table.concat(u, "/")
+		elseif cfg.lower == 3 then
+			self.urls[k] = v:lower()
+		end
+		-- prepend http:// to URLs with no prefix for better dupe removal
+		if not v:match("://") then self.urls[k] = "http://"..v end
+	end
+	-- remove duplicates
+	self.urls = table.uniq(self.urls)
+end
+
+-- check min/max URLs in the config against self.urls, and open them
+function frmUrls:OpenUrls(urls)
+	if cfg.conf_urls and #urls >= cfg.conf_urls then
+		local ok = wx.wxMessageBox(
+			"OK to open "..#urls.." URLs?", "Confirm", wx.wxYES_NO
+		)
+		if ok == wx.wxNO then return end
+	end
+	for _,v in pairs(urls) do wx.wxLaunchDefaultBrowser(v) end
+	self.urls = {}
+end
+
+-- destroy this frame
+function frmUrls:Close()
+	self.urls = {}
+	self.frame:Destroy()
+	self.frame = nil
+end
+
+return frmUrls
---
+++ main.lua
@@ -0,0 +1,133 @@
+-- handlr 0.4.0 (20120416)
+-- http://b23.be/handlr
+-- strawman at b23 dot be
+
+-- TODO:
+-- ~~ use wxConfig instead of pl.pretty?
+-- ++ write tests
+
+function table.contains(tbl, val)
+  for _,v in pairs(tbl) do
+    if v == val then return true end
+  end
+  return false
+end
+
+function table.uniq(tbl)
+  local new = {}
+  for _,v in pairs(tbl) do
+    if not table.contains(new, v) then table.insert(new, v) end
+  end
+  return new
+end
+
+function string.split(str)
+  new = {}
+  for str in str:gmatch("%w+") do table.insert(new,str) end
+  return new
+end
+
+--
+
+package.path = package.path..";D:/mirror/dev/handlr/?.lua"  -- deleteme
+
+require("wx")
+local frmUrls     = require("frmUrls")
+local frmSettings = require("frmSettings")
+local pp          = require("pl.pretty")
+local PORTABLE    = true
+
+-- config
+
+cfg = {
+  auto_urls  = 2,
+  conf_urls  = 10,
+  prefixes   = {"http", "https"},
+  subdomains = {"www"},
+  attrs      = {"href", "src"},
+  convert    = 0
+}
+
+setmetatable(cfg, {__index = {
+  file = nil,
+  dir = function()
+    return PORTABLE and 
+    wx.wxStandardPaths.Get():GetLocalDataDir() or
+    wx.wxStandardPaths.Get():GetUserDataDir()
+  end,
+  update = function(self,tbl)
+    for k,v in pairs(tbl) do self[k] = v end
+  end,
+  load = function(self, file)
+    if not file then file = self.dir() .. "/handlr.cfg" end
+    self.file = file
+    local cnf = io.open(file, "r")
+    if not cnf then return end
+    local str = cnf:read("*all")
+    cnf:close()
+    local tbl = pp.read(str)
+    if not tbl then return end
+    self:update(tbl)
+  end,
+  save = function(self, file)
+    if not file then file = self.file end
+    pp.dump(self, file)
+    frmUrls:BuildSearch()
+  end
+}})
+
+-- tray icon
+
+local trayicon = wx.wxTaskBarIcon()
+function trayicon:init()
+  self:SetIcon(wx.wxIcon("HANDLR", wx.wxBITMAP_TYPE_ICO_RESOURCE), "handlr")
+
+  self.menu = wx.wxMenu()
+  self.menu_opts  = wx.wxMenuItem(self.menu, wx.wxID_ANY, "Settings")
+  self.menu_about = wx.wxMenuItem(self.menu, wx.wxID_ANY, "About")
+  self.menu_quit  = wx.wxMenuItem(self.menu, wx.wxID_ANY, "Quit")
+  self.menu:Append(self.menu_opts)
+  self.menu:Append(self.menu_about)
+  self.menu:Append(self.menu_quit)
+
+  self:Connect(wx.wxEVT_TASKBAR_RIGHT_DOWN, 
+    function(e)
+      self.PopupMenu(self, self.menu)
+    end
+  )
+
+  self:Connect(wx.wxEVT_TASKBAR_LEFT_DOWN,
+    function(e) 
+      frmUrls:GetUrls()
+      if #frmUrls.urls == 0 then
+        -- show balloon notification
+        return
+      end
+      if cfg.auto_urls and #frmUrls.urls <= cfg.auto_urls then
+        frmUrls:OpenUrls(frmUrls.urls)
+        return
+      end
+      frmUrls:Show()
+    end
+  )
+
+  self:Connect(self.menu_opts:GetId(), wx.wxEVT_COMMAND_MENU_SELECTED,
+    function(e) frmSettings:Show() end
+  )
+
+  self:Connect(self.menu_about:GetId(), wx.wxEVT_COMMAND_MENU_SELECTED,
+    function(e) wx.wxLaunchDefaultBrowser("http://b23.be/handlr") end
+  )
+
+  self:Connect(self.menu_quit:GetId(), wx.wxEVT_COMMAND_MENU_SELECTED,
+    function(e) wx.wxGetApp():ExitMainLoop() end
+  )
+
+end
+
+-- let's go
+
+cfg:load()
+frmUrls:BuildSearch()
+trayicon:init()
+wx.wxGetApp():MainLoop()
